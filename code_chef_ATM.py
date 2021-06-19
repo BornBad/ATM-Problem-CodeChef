5a58ea301e71cf5853a0bc60017c8d9d5530a904
BornBad <33731154+BornBad@users.noreply.github.com>
2021-06-20 00:10:55 +0530

    Update code_chef_ATM.py

--- code_chef_ATM.py
+++ code_chef_ATM.py
@@ -2,9 +2,9 @@ def atm(withdrawl_amount, total_amount):
     if((withdrawl_amount or total_amount)<=2000):
         if(withdrawl_amount%5==0 and withdrawl_amount<total_amount):
             closing_amount=total_amount-(withdrawl_amount+0.50)
-            print('%.3f'%closing_amount)
+            print('%.2f'%closing_amount)
         elif(withdrawl_amount%5!=0 or withdrawl_amount>=total_amount):
-            print('%.3f'%total_amount)
+            print('%.2f'%total_amount)
     else:
         print("You can not operate as you are exceeding the limit")
     return withdrawl_amount,total_amount
